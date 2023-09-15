a0a5042aab228179155a35e5c94613381c225a72
OVP2023 <138027862+OVP2023@users.noreply.github.com>
2023-09-16 00:11:02 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './vite.svg'
+import './index.css'
+
+function App(props) {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App">
+        <div className="main-content">
+          <h2>{props.brand}</h2>
+          <h1>{props.title}</h1>
+          <h3>{props.description}</h3>
+          <div className="description">{props.descriptionFull}</div>
+          <div className="highlight-window mobile">
+            <div className="highlight-overlay"></div>
+          </div>
+          <div className="divider"></div>
+          <div className="purchase-info">
+            <div className="price">
+              {props.currency}
+              {props.price}
+            </div>
+            <button>Добавить в корзину</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
